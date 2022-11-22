a0a7b83bee2dc4e3eda914ba69a61ac4da76c0f4
Roy Lopez <windup.dues-0f@icloud.com>
2022-11-22 20:24:32 -0300

    addItemToCart functionality added

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,46 @@
 import { createContext, useState } from "react";
 
+const addCartItem = (cartItems, productToAdd) => {
+  // Find if cart items contains productToAdd
+  const existingProd = cartItems.find(
+    cartItem => cartItem.id === productToAdd.id
+  );
+
+  // Is found, increment quatity
+  if (existingProd) {
+    return cartItems.map(cartItem =>
+      cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+
+  // Return new array with modified cart cartItems
+  return [
+    ...cartItems,
+    {
+      ...productToAdd,
+      quantity: 1,
+    },
+  ];
+};
+
 export const CartContext = createContext({
   displayCart: false,
   setDisplayCart: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
 });
 
 export const CartProvider = ({ children }) => {
   const [displayCart, setDisplayCart] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
 
-  const value = { displayCart, setDisplayCart };
+  const addItemToCart = productToAdd => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  };
+
+  const value = { displayCart, setDisplayCart, cartItems, addItemToCart };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
